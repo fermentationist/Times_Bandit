565c0515779e5fd127f3e89e164d2f5a32ec9b9f
Dennis Hodges <fermentationist@gmail.com>
2019-03-15 03:17:51 -0500

    Deleted contentScript.js

--- contentScript.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    if (request.message === "clear storage") {
-        localStorage.clear();
-        sessionStorage.clear();
-        chrome.runtime.sendMessage({message: "storage cleared."});
-    }
-    return true;
-    
-});
